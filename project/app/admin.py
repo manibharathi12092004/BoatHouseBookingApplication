9da14c227a6d5144241d9e9a65dd57147d2e13c8
manibharathi12092004 <124987412+manibharathi12092004@users.noreply.github.com>
2024-08-13 12:01:52 +0530

    Delete project directory

--- project/app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import BoatData, User, Admin, Booking, Boat
-
-# Register your models here
-admin.site.register(BoatData)
-admin.site.register(User)
-admin.site.register(Admin)
-admin.site.register(Booking)
-admin.site.register(Boat)
